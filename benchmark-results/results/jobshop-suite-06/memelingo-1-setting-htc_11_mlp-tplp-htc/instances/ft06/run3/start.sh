bb006b898c80ebde5d674af5333f5cd7e2c5923f
hahnmartinlu <hahnmartinlu@hpc-login.hpc.cs.uni-potsdam.de>
2026-02-09 18:47:20 +0100

    Results from weekend

---
+++ benchmark-results/results/jobshop-suite-06/memelingo-1-setting-htc_11_mlp-tplp-htc/instances/ft06/run3/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# https://github.com/arminbiere/runlim
+
+CAT="../../../../../../../../../programs/gcat.sh"
+
+cd "$(dirname $0)"
+
+runner=( "../../../../../../../../../programs/runlim" \
+  --single \
+  --space-limit=20000 \
+  --output-file=runsolver.watcher \
+  --real-time-limit=1200 \
+  "../../../../../../../../../programs/memelingo-1" --project=show --stats -c lambda=11 --approach=mlp-tplp-htc)
+
+input=( "../../../../../../../../../../examples/benchmarks/job-shop/instances/ft06.lp" "../../../../../../../../../../examples/benchmarks/job-shop/job-shop.lp" )
+
+if [[ ! -e .finished ]]; then
+  {
+    if file -b --mime-type -L  "${input[@]}" | grep -qv "text/"; then
+      "$CAT" "${input[@]}" | "${runner[@]}"
+    else
+      "${runner[@]}" "${input[@]}"
+    fi
+  } > runsolver.solver
+fi
+
+touch .finished
