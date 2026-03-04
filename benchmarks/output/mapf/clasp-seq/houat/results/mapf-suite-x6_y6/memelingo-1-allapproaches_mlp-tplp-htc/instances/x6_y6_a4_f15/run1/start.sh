0589fe6f640f28d37cead4dfc9a6520bd0b4d4b3
hahnmartinlu <hahnmartinlu@hpc-login.hpc.cs.uni-potsdam.de>
2026-03-04 15:51:31 +0100

    Results v7

---
+++ benchmarks/output/mapf/clasp-seq/houat/results/mapf-suite-x6_y6/memelingo-1-allapproaches_mlp-tplp-htc/instances/x6_y6_a4_f15/run1/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# https://github.com/arminbiere/runlim
+
+CAT="../../../../../../../../../../programs/gcat.sh"
+
+cd "$(dirname $0)"
+
+runner=( "../../../../../../../../../../programs/runlim" \
+   \
+  --space-limit=20000 \
+  --output-file=runsolver.watcher \
+  --real-time-limit=1200 \
+  "../../../../../../../../../../programs/memelingo-1" \
+  --models=1 --project=show --stats -c lambda=10 --approach=mlp-tplp-htc -c v=225 \
+     )
+
+input=( "../../../../../../../../../../../examples/mapf/instances/x6_y6_a4_f15.lp" "../../../../../../../../../../../examples/mapf/mapf.lp" )
+
+if [[ ! -e .finished ]]; then
+  {
+    if file -b --mime-type -L  "${input[@]}" | grep -qv "text/"; then
+      "$CAT" "${input[@]}" | "${runner[@]}"
+    else
+      "${runner[@]}" "${input[@]}"
+    fi
+  } > runsolver.solver
+fi
+
+touch .finished
